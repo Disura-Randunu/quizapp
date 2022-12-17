21670b9d320c38fb7970982a9bd21f30ca5a3d41
Disura Randunu <37591051+Disura-Randunu@users.noreply.github.com>
2022-12-17 14:53:06 +0530

    live db config added

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'live';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -95,3 +95,28 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['live'] = array(
+	'dsn'	=> '',
+	'hostname' => 'phpmyadmin.ecs.westminster.ac.uk',
+	'port' => 3306,
+	'username' => 'w1761865',
+	'password' => 'XjgWwpD9vHD8',
+	'database' => 'w1761865_0',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'), 
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+
